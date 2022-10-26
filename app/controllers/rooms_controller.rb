5d51f3fb8fb05be80a14a6367a9ef2991e0bee75
Carlos Alfonso Garcia Gonzales <cgarcia@stsac.com>
2022-10-26 17:32:19 -0500

    Creating action

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,35 @@
 class RoomsController < ApplicationController
+before_action :set_room, only: %i[show]
+    def index
+        @rooms = Room.all
+    end
+    
+    def show
+
+    end
+
+    def new
+        @room = Room.new
+    end
+
+    def create
+        @room = Room.new(room_params)
+        respond_to do |format|
+            if @room.save
+                format.html {redirect_to room_url(@room), notice: "Room was created succesfully"}
+            else
+                format.html{render :new, status: :unprocessable_entity}
+            end
+        end
+    end
+
+    private
+
+    def set_room
+        @room = Room.find(params[:id])
+    end
+
+    def room_params
+        params.require(:room).permit(:name, :capacity)
+    end
 end

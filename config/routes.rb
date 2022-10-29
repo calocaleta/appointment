c410f3ac99a21188ec8d49ffe3b4df58ac537d2d
Carlos Alfonso Garcia Gonzales <cgarcia@stsac.com>
2022-10-29 18:11:35 -0500

    Adding Bookings

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   root "rooms#index"
+  root "rooms#index"
   resources :rooms
+  resources :bookings
 end
